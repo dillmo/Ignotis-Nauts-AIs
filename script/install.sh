2d8f7873762bd5318c3b673b8189dcfe319b65be
Dillon Morse <dillonm662@gmail.com>
2014-10-25 06:36:39 -0400

    Update scripts to only copy .xml files

--- script/install.sh
+++ script/install.sh
@@ -4,24 +4,24 @@ DEST=~/.local/share/Steam/SteamApps/common/Awesomenauts/Data/BehavioursCustom/
 rm "$DEST"/*
 
 if [ "$(echo "$*" | grep -c "assassin")" -gt 0 ]; then
-  cp -a Unstoppable\ Fox/. "$DEST"
+  cp -a Unstoppable\ Fox/*.xml "$DEST"
 fi
 
 if [ "$(echo "$*" | grep -c "summoner")" -gt 0 ]; then
-  cp -a BrainyVoltar/. "$DEST"
+  cp -a BrainyVoltar/*.xml "$DEST"
 fi
 
 if [ "$(echo "$*" | grep -c "spy")" -gt 0 ]; then
-  cp -a Super\ Sentry/. "$DEST"
+  cp -a Super\ Sentry/*.xml "$DEST"
 fi
 
 if [ "$(echo "$*" | grep -c "jetter")" -gt 0 ]; then
-  cp -a YuriAI/. "$DEST"
+  cp -a YuriAI/*.xml "$DEST"
 fi
 
 if [ -z "$*" ]; then
-  cp -a Unstoppable\ Fox/. "$DEST"
-  cp -a BrainyVoltar/. "$DEST"
-  cp -a Super\ Sentry/. "$DEST"
-  cp -a YuriAI/. "$DEST"
+  cp -a Unstoppable\ Fox/*.xml "$DEST"
+  cp -a BrainyVoltar/*.xml "$DEST"
+  cp -a Super\ Sentry/*.xml "$DEST"
+  cp -a YuriAI/*.xml "$DEST"
 fi
--- script/package.sh
+++ script/package.sh
@@ -5,15 +5,15 @@ fi
 
 # BrainyVoltar
 cd BrainyVoltar
-zip ../Release/BrainyVoltar.zip *
+zip ../Release/BrainyVoltar.zip *.xml
 cd ../
 
 # Super Sentry
 cd Super\ Sentry
-zip ../Release/Super\ Sentry.zip *
+zip ../Release/Super\ Sentry.zip *.xml
 cd ../
 
 # Unstoppable Fox
 cd Unstoppable\ Fox
-zip ../Release/Unstoppable\ Fox.zip *
+zip ../Release/Unstoppable\ Fox.zip *.xml
 cd ../
