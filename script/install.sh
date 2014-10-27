1291a74b8330e50f152bf0692620ca48e60a4ab5
Dillon Morse <dillonm662@gmail.com>
2014-10-27 08:00:11 -0400

    Create patches for tracking changes to Unstoppable Fox and Super Sentry across gamemodes

--- script/install.sh
+++ script/install.sh
@@ -20,7 +20,15 @@ if [ "$(echo "$*" | grep -c "summoner")" -gt 0 ]; then
 fi
 
 if [ "$(echo "$*" | grep -c "spy")" -gt 0 ]; then
-  cp -a Super\ Sentry/*.xml "$DEST"
+  if [ "$(echo "$*" | grep -c "default")" -gt 0 ]; then
+    cp -a Super\ Sentry/default/*.xml "$DEST"
+  elif [ "$(echo "$*" | grep -c "deathmatch")" -gt 0 ]; then
+    cp -a Super\ Sentry/deathmatch/*.xml "$DEST"
+  elif [ "$(echo "$*" | grep -c "bossmode")" -gt 0 ]; then
+    cp -a Super\ Sentry/bossmode/*.xml "$DEST"
+  else
+    cp -a Super\ Sentry/default/*.xml "$DEST"
+  fi
 fi
 
 if [ "$(echo "$*" | grep -c "jetter")" -gt 0 ]; then
@@ -30,6 +38,6 @@ fi
 if [ -z "$*" ]; then
   cp -a Unstoppable\ Fox/default/*.xml "$DEST"
   cp -a BrainyVoltar/*.xml "$DEST"
-  cp -a Super\ Sentry/*.xml "$DEST"
+  cp -a Super\ Sentry/default/*.xml "$DEST"
   cp -a YuriAI/*.xml "$DEST"
 fi
