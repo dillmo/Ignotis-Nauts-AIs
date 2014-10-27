797460cbdab8227dd3da99dc6f809f02ff13a733
Dillon Morse <dillonm662@gmail.com>
2014-10-27 19:13:12 -0400

    Update install script for YuriAI's new directory structure

--- script/install.sh
+++ script/install.sh
@@ -19,25 +19,25 @@ if [ "$(echo "$*" | grep -c "summoner")" -gt 0 ]; then
   cp -a BrainyVoltar/*.xml "$DEST"
 fi
 
-if [ "$(echo "$*" | grep -c "spy")" -gt 0 ]; then
-  if [ "$(echo "$*" | grep -c "default")" -gt 0 ]; then
-    cp -a Super\ Sentry/default/*.xml "$DEST"
-  elif [ "$(echo "$*" | grep -c "deathmatch")" -gt 0 ]; then
-    cp -a Super\ Sentry/deathmatch/*.xml "$DEST"
-  elif [ "$(echo "$*" | grep -c "bossmode")" -gt 0 ]; then
-    cp -a Super\ Sentry/bossmode/*.xml "$DEST"
-  else
-    cp -a Super\ Sentry/default/*.xml "$DEST"
-  fi
+if [ "$(echo "$*" | grep -c "jetter")" -gt 0 ]; then
+  cp -a YuriAI/*.xml "$DEST"
 fi
 
 if [ "$(echo "$*" | grep -c "jetter")" -gt 0 ]; then
-  cp -a YuriAI/*.xml "$DEST"
+  if [ "$(echo "$*" | grep -c "default")" -gt 0 ]; then
+    cp -a YuriAI/default/*.xml "$DEST"
+  elif [ "$(echo "$*" | grep -c "deathmatch")" -gt 0 ]; then
+    cp -a YuriAI/deathmatch/*.xml "$DEST"
+  elif [ "$(echo "$*" | grep -c "bossmode")" -gt 0 ]; then
+    cp -a YuriAI/bossmode/*.xml "$DEST"
+  else
+    cp -a YuriAI/default/*.xml "$DEST"
+  fi
 fi
 
 if [ -z "$*" ]; then
   cp -a Unstoppable\ Fox/default/*.xml "$DEST"
   cp -a BrainyVoltar/*.xml "$DEST"
   cp -a Super\ Sentry/default/*.xml "$DEST"
-  cp -a YuriAI/*.xml "$DEST"
+  cp -a YuriAI/default/*.xml "$DEST"
 fi
