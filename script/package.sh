5313432326ecdfe9ca5d3b6d080da41b2c381fbd
Dillon Morse <dillonm662@gmail.com>
2014-10-31 20:57:54 -0400

    Update package script to match current directory structure

--- script/package.sh
+++ script/package.sh
@@ -4,16 +4,33 @@ if [ ! -d Release ]; then
 fi
 
 # BrainyVoltar
+if [ -f Release/BrainyVoltar.zip ]; then
+  rm Release/BrainyVoltar.zip
+fi
 cd BrainyVoltar
-zip ../Release/BrainyVoltar.zip *.xml
+zip ../Release/BrainyVoltar.zip ./*/*.xml
 cd ../
 
 # Super Sentry
+if [ -f Release/Super\ Sentry.zip ]; then
+  rm Release/Super\ Sentry.zip
+fi
 cd Super\ Sentry
-zip ../Release/Super\ Sentry.zip *.xml
+zip ../Release/Super\ Sentry.zip ./*/*.xml
 cd ../
 
 # Unstoppable Fox
+if [ -f Release/Unstoppable\ Fox.zip ]; then
+  rm Release/Unstoppable\ Fox.zip
+fi
 cd Unstoppable\ Fox
-zip ../Release/Unstoppable\ Fox.zip *.xml
+zip ../Release/Unstoppable\ Fox.zip ./*/*.xml
+cd ../
+
+# YuriAI
+if [ -f Release/YuriAI.zip ]; then
+  rm Release/YuriAI.zip
+fi
+cd YuriAI
+zip ../Release/YuriAI.zip ./*/*.xml
 cd ../
