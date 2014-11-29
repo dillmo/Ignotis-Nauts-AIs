50e7ac3e94354ab095eefb8de6e7ef4991457cd1
Dillon Morse <dillonm662@gmail.com>
2014-11-29 17:08:22 -0500

    Modify install script for new directory structure

--- script/install.sh
+++ script/install.sh
@@ -4,30 +4,14 @@ DEST=~/.local/share/Steam/SteamApps/common/Awesomenauts/Data/BehavioursCustom/
 rm "$DEST"/*
 
 if [ "$(echo "$*" | grep -c "spy")" -gt 0 ]; then
-  if [ "$(echo "$*" | grep -c "default")" -gt 0 ]; then
-    cp -a Super\ Sentry/default/*.xml "$DEST"
-  elif [ "$(echo "$*" | grep -c "deathmatch")" -gt 0 ]; then
-    cp -a Super\ Sentry/deathmatch/*.xml "$DEST"
-  elif [ "$(echo "$*" | grep -c "bossmode")" -gt 0 ]; then
-    cp -a Super\ Sentry/bossmode/*.xml "$DEST"
-  else
-    cp -a Super\ Sentry/default/*.xml "$DEST"
-  fi
+  cp -a Super\ Sentry/*.xml "$DEST"
 fi
 
 if [ "$(echo "$*" | grep -c "jetter")" -gt 0 ]; then
-  if [ "$(echo "$*" | grep -c "default")" -gt 0 ]; then
-    cp -a YuriAI/default/*.xml "$DEST"
-  elif [ "$(echo "$*" | grep -c "deathmatch")" -gt 0 ]; then
-    cp -a YuriAI/deathmatch/*.xml "$DEST"
-  elif [ "$(echo "$*" | grep -c "bossmode")" -gt 0 ]; then
-    cp -a YuriAI/bossmode/*.xml "$DEST"
-  else
-    cp -a YuriAI/default/*.xml "$DEST"
-  fi
+  cp -a YuriAI/*.xml "$DEST"
 fi
 
 if [ -z "$*" ]; then
-  cp -a Super\ Sentry/default/*.xml "$DEST"
-  cp -a YuriAI/default/*.xml "$DEST"
+  cp -a Super\ Sentry/*.xml "$DEST"
+  cp -a YuriAI/*.xml "$DEST"
 fi
