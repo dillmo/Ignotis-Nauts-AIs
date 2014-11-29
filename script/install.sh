3ab15a33923f3a6c1067ba72f3e4dc29ce149b90
Dillon Morse <dillonm662@gmail.com>
2014-11-29 12:14:36 -0500

    Remove AI's that did not make it into the beta

--- script/install.sh
+++ script/install.sh
@@ -3,30 +3,6 @@ DEST=~/.local/share/Steam/SteamApps/common/Awesomenauts/Data/BehavioursCustom/
 
 rm "$DEST"/*
 
-if [ "$(echo "$*" | grep -c "assassin")" -gt 0 ]; then
-  if [ "$(echo "$*" | grep -c "default")" -gt 0 ]; then
-    cp -a Unstoppable\ Fox/default/*.xml "$DEST"
-  elif [ "$(echo "$*" | grep -c "deathmatch")" -gt 0 ]; then
-    cp -a Unstoppable\ Fox/deathmatch/*.xml "$DEST"
-  elif [ "$(echo "$*" | grep -c "bossmode")" -gt 0 ]; then
-    cp -a Unstoppable\ Fox/bossmode/*.xml "$DEST"
-  else
-    cp -a Unstoppable\ Fox/default/*.xml "$DEST"
-  fi
-fi
-
-if [ "$(echo "$*" | grep -c "summoner")" -gt 0 ]; then
-  if [ "$(echo "$*" | grep -c "default")" -gt 0 ]; then
-    cp -a BrainyVoltar/default/*.xml "$DEST"
-  elif [ "$(echo "$*" | grep -c "deathmatch")" -gt 0 ]; then
-    cp -a BrainyVoltar/deathmatch/*.xml "$DEST"
-  elif [ "$(echo "$*" | grep -c "bossmode")" -gt 0 ]; then
-    cp -a BrainyVoltar/bossmode/*.xml "$DEST"
-  else
-    cp -a BrainyVoltar/default/*.xml "$DEST"
-  fi
-fi
-
 if [ "$(echo "$*" | grep -c "spy")" -gt 0 ]; then
   if [ "$(echo "$*" | grep -c "default")" -gt 0 ]; then
     cp -a Super\ Sentry/default/*.xml "$DEST"
@@ -52,8 +28,6 @@ if [ "$(echo "$*" | grep -c "jetter")" -gt 0 ]; then
 fi
 
 if [ -z "$*" ]; then
-  cp -a Unstoppable\ Fox/default/*.xml "$DEST"
-  cp -a BrainyVoltar/default/*.xml "$DEST"
   cp -a Super\ Sentry/default/*.xml "$DEST"
   cp -a YuriAI/default/*.xml "$DEST"
 fi
--- script/package.sh
+++ script/package.sh
@@ -3,28 +3,12 @@ if [ ! -d Release ]; then
   mkdir Release
 fi
 
-# BrainyVoltar
-if [ -f Release/BrainyVoltar.zip ]; then
-  rm Release/BrainyVoltar.zip
-fi
-cd BrainyVoltar
-zip ../Release/BrainyVoltar.zip ./*.xml
-cd ../
-
 # Super Sentry
 if [ -f Release/Super\ Sentry.zip ]; then
   rm Release/Super\ Sentry.zip
 fi
 cd Super\ Sentry
 zip ../Release/Super\ Sentry.zip ./*.xml
-cd ../
-
-# Unstoppable Fox
-if [ -f Release/Unstoppable\ Fox.zip ]; then
-  rm Release/Unstoppable\ Fox.zip
-fi
-cd Unstoppable\ Fox
-zip ../Release/Unstoppable\ Fox.zip ./*.xml
 cd ../
 
 # YuriAI
