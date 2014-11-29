2714563c043c64e91b3cd532a27020b07eb76a7a
Dillon Morse <dillonm662@gmail.com>
2014-11-29 12:11:41 -0500

    Modify package script for new directory structure

--- script/package.sh
+++ script/package.sh
@@ -8,7 +8,7 @@ if [ -f Release/BrainyVoltar.zip ]; then
   rm Release/BrainyVoltar.zip
 fi
 cd BrainyVoltar
-zip ../Release/BrainyVoltar.zip ./*/*.xml
+zip ../Release/BrainyVoltar.zip ./*.xml
 cd ../
 
 # Super Sentry
@@ -16,7 +16,7 @@ if [ -f Release/Super\ Sentry.zip ]; then
   rm Release/Super\ Sentry.zip
 fi
 cd Super\ Sentry
-zip ../Release/Super\ Sentry.zip ./*/*.xml
+zip ../Release/Super\ Sentry.zip ./*.xml
 cd ../
 
 # Unstoppable Fox
@@ -24,7 +24,7 @@ if [ -f Release/Unstoppable\ Fox.zip ]; then
   rm Release/Unstoppable\ Fox.zip
 fi
 cd Unstoppable\ Fox
-zip ../Release/Unstoppable\ Fox.zip ./*/*.xml
+zip ../Release/Unstoppable\ Fox.zip ./*.xml
 cd ../
 
 # YuriAI
@@ -32,5 +32,5 @@ if [ -f Release/YuriAI.zip ]; then
   rm Release/YuriAI.zip
 fi
 cd YuriAI
-zip ../Release/YuriAI.zip ./*/*.xml
+zip ../Release/YuriAI.zip ./*.xml
 cd ../
